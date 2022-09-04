0998fcd65727ba74005133232801f351fe90d5a9
Bas Hopman <bas@familiehopman.net>
2022-09-04 13:47:29 +0200

    initial transip

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.16
 
 require (
 	github.com/caddyserver/caddy/v2 v2.4.1
-	github.com/libdns/template v0.1.0
+	github.com/libdns/transip v0.0.0-20211011153753-1c0ec90e7a12
 )
--- module.go
+++ module.go
@@ -1,15 +1,13 @@
 package template
 
 import (
-	"fmt"
-
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
-	libdnstemplate "github.com/libdns/template"
+	transip "github.com/libdns/transip"
 )
 
 // Provider lets Caddy read and manipulate DNS records hosted by this DNS provider.
-type Provider struct{ *libdnstemplate.Provider }
+type Provider struct{ *transip.Provider }
 
 func init() {
 	caddy.RegisterModule(Provider{})
@@ -18,54 +16,54 @@ func init() {
 // CaddyModule returns the Caddy module information.
 func (Provider) CaddyModule() caddy.ModuleInfo {
 	return caddy.ModuleInfo{
-		ID:  "dns.providers.template",
-		New: func() caddy.Module { return &Provider{new(libdnstemplate.Provider)} },
+		ID:  "dns.providers.transip",
+		New: func() caddy.Module { return &Provider{new(transip.Provider)} },
 	}
 }
 
-// TODO: This is just an example. Useful to allow env variable placeholders; update accordingly.
 // Provision sets up the module. Implements caddy.Provisioner.
 func (p *Provider) Provision(ctx caddy.Context) error {
-	p.Provider.APIToken = caddy.NewReplacer().ReplaceAll(p.Provider.APIToken, "")
-	return fmt.Errorf("TODO: not implemented")
+	p.Provider.AccountName = caddy.NewReplacer().ReplaceAll(p.Provider.AccountName, "")
+	p.Provider.PrivateKeyPath = caddy.NewReplacer().ReplaceAll(p.Provider.PrivateKeyPath, "")
+	return nil
 }
 
 // TODO: This is just an example. Update accordingly.
 // UnmarshalCaddyfile sets up the DNS provider from Caddyfile tokens. Syntax:
 //
-// providername [<api_token>] {
-//     api_token <api_token>
-// }
+//	providername [<api_token>] {
+//	    api_token <api_token>
+//	}
 //
 // **THIS IS JUST AN EXAMPLE AND NEEDS TO BE CUSTOMIZED.**
 func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
-	for d.Next() {
-		if d.NextArg() {
-			p.Provider.APIToken = d.Val()
-		}
-		if d.NextArg() {
-			return d.ArgErr()
-		}
-		for nesting := d.Nesting(); d.NextBlock(nesting); {
-			switch d.Val() {
-			case "api_token":
-				if p.Provider.APIToken != "" {
-					return d.Err("API token already set")
-				}
-				if d.NextArg() {
-					p.Provider.APIToken = d.Val()
-				}
-				if d.NextArg() {
-					return d.ArgErr()
-				}
-			default:
-				return d.Errf("unrecognized subdirective '%s'", d.Val())
-			}
-		}
-	}
-	if p.Provider.APIToken == "" {
-		return d.Err("missing API token")
-	}
+	//for d.Next() {
+	//	if d.NextArg() {
+	//		p.Provider.APIToken = d.Val()
+	//	}
+	//	if d.NextArg() {
+	//		return d.ArgErr()
+	//	}
+	//	for nesting := d.Nesting(); d.NextBlock(nesting); {
+	//		switch d.Val() {
+	//		case "api_token":
+	//			if p.Provider.APIToken != "" {
+	//				return d.Err("API token already set")
+	//			}
+	//			if d.NextArg() {
+	//				p.Provider.APIToken = d.Val()
+	//			}
+	//			if d.NextArg() {
+	//				return d.ArgErr()
+	//			}
+	//		default:
+	//			return d.Errf("unrecognized subdirective '%s'", d.Val())
+	//		}
+	//	}
+	//}
+	//if p.Provider.APIToken == "" {
+	//	return d.Err("missing API token")
+	//}
 	return nil
 }
 
